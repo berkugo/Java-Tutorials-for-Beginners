1f78fb89dbc6a778cb681d44ff00714153bc241d
ugurogluberk <berk.ugo@gmail.com>
2020-04-21 03:37:10 -0700

    First week | Fundamentals

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+
+
+public static void Main(String[] args) {
+    
+    System.out.println(args.lenght);
+    int total = Integer.parseInt(args[0] + args[1]);
+    System.out.println("Sonuç:" + total);
+
+    /*
+    Scanner sc = new Scanner(System.in);
+    int first = sc.nextInt();
+    int second = sc.nextInt();
+    sc.close();
+    // System.out.println("Sonuç:" + (first + second));
+    // System.out.println("Sonuç:" + first + second); // learn the difference between this line and the one up
+    // Search about the difference between Float and Double
+
+}
